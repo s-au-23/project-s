02e4eb9fd9bcbe19e4707c7dc00ebcd7ec967e6e
Gauri Suryawanshi <suryawanshigauri23@gmail.com>
2026-02-11 23:16:50 +0530

    port is added to backend

--- backend/app.py
+++ backend/app.py
@@ -321,8 +321,10 @@ def get_feedbacks_admin():
         "date": f.date
     } for f in feedbacks])
 
-# --- START SERVER ---
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
