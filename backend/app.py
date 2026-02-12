fad8b2953468c39a04c795736970ec248c193b3e
Gauri Suryawanshi <suryawanshigauri23@gmail.com>
2026-02-12 13:05:19 +0530

    add port

--- backend/app.py
+++ backend/app.py
@@ -266,6 +266,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all() 
         print("🚀 Database Tables Created!")
+        port_str = os.environ.get("PORT", "5000")
+        port = int(port_str) 
     
-    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
